a5c5316d0404625a3836a25627a5d5cefea2a4bf
Alejandro Pacheco <alepacheco99@gmail.com>
2017-05-02 23:57:01 +0200

    Inplemented as a library

--- main.go
+++ main.go
@@ -3,22 +3,12 @@ package main
 
 import (
 	"fmt"
-	"syscall"
+	"github.com/alepacheco/Process-Protection/process-protection"
 )
-
-var (
-	ntdll                   = syscall.MustLoadDLL("ntdll.dll")
-	NtSetInformationProcess = ntdll.MustFindProc("NtSetInformationProcess")
-)
-
-func SetInformationProcess(hProcess uintptr, processInformationClass int, processInformation int, processInformationLength int) {
-	_, _, _ = NtSetInformationProcess.Call(hProcess, uintptr(processInformationClass), uintptr(processInformation), uintptr(processInformationLength))
-}
 
 func main() {
 	fmt.Println("This program will protect its-self.")
-	me, _ := syscall.GetCurrentProcess()
-	SetInformationProcess(uintptr(me), 29, 1, 4)
+	process-protection.Protect()
 	fmt.Println("Try killing me with a unelevated tool.")
 	for {
 	}
